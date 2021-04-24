139fb7245efa1274b4ef1a760683fd18cfe7c9bd
Vannda <vandakol222@gmail.com>
2021-04-24 22:53:41 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cheetah Coding - Flutter
+
+This is the repository for the [Cheetah Coding](https://www.youtube.com/cheetahcoding??sub_confirmation=1) Youtube channel.
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:CWCFlutter/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Color(0xFF2F5233)),
+        primaryColor: Color(0xFF2F5233),
+        backgroundColor: Color(0xFFDCDCDC),
+      ),
+      home: Home(),
+    );
+  }
+}
